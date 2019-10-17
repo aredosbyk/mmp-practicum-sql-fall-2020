7f1c8705e0a15f1c879d630739a9760853030185
Nikolay Korolev <korolevns98@gmail.com>
2019-10-17 19:55:38 +0300

    Something

--- util/modules/cache_manager.py
+++ util/modules/cache_manager.py
@@ -8,11 +8,17 @@ os.makedirs(CACHE_DIRECTORY, mode=0o777, exist_ok=True)
 now = datetime.datetime.now()
 
 
-def get_cached_query(filename):
-	if not filename.endswith(".sql"):
+def get_cached_query(file_path):
+	if not file_path.endswith(".sql") or not os.path.isfile(file_path):
 		return {}
-	filename = filename[:-4]
-	full_path = os.path.join(CACHE_DIRECTORY, filename, ".json")
+	with open(file_path, 'r', encoding='utf-8') as f:
+        content = "".join(f.readlines()).replace(';', '')
+	file_path = file_path[:-4]
+	full_path = os.path.join(CACHE_DIRECTORY, file_path, ".json")
 	if not os.path.isfile(full_path):
 		return {}
-	return json.load(open(full_path))
+	cached_query = json.load(open(full_path))
+	if cached_query['valid_until'] < now or cached_query['sql'] != content:
+		os.remove(full_path)
+		return {}
+	return cached_query
